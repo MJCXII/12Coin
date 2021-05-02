7f7e966b7dd3e5b82a97aae641af21f48ce8f2ce
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-02 19:03:10 +0100

    styled sign up button

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,8 +6,11 @@ import Dropdown from "./Dropdown";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -17,6 +20,34 @@ function Navbar() {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              to="/get-twxiive"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Use TWXIIVE <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/resources"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Resources <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link to="/sign-up" className="nav-links" onClick={closeMobileMenu}>
+              Sign Up <i className="nav-links-mobile" />
+            </Link>
+          </li>
+        </ul>
+        <Button />
       </nav>
     </>
   );
