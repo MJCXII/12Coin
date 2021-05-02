ac7f09a85bf53ae3d83ac53c37b9e767c14a3726
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-02 20:40:27 +0100

    implementing dropdown

---
+++ frontend/src/components/Dropdown.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import "./Dropdown/css";
+
+function Dropdown() {
+  return <></>;
+}
+
+export default Dropdown;
---
+++ frontend/src/components/MenuItem.js
@@ -0,0 +1,7 @@
+export const MenuItems = [
+  {
+    title: "Wallets",
+    path: "/wallets",
+    cName: "dropdown-Link",
+  },
+];
