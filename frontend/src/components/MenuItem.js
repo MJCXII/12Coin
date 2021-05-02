667145ed55a14037105bab1ca18b2df811de2b42
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-02 20:46:00 +0100

    updated menuitems

--- frontend/src/components/MenuItem.js
+++ frontend/src/components/MenuItem.js
@@ -4,4 +4,14 @@ export const MenuItems = [
     path: "/wallets",
     cName: "dropdown-Link",
   },
+  {
+    title: "Buy TWXIIVE",
+    path: "/buy-twxiive",
+    cName: "dropdown-Link",
+  },
+  {
+    title: "Sell TWXIIVE",
+    path: "/sell-twxiive",
+    cName: "dropdown-Link",
+  },
 ];
